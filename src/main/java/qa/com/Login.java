dfcbae8de6970752c1478f53b77936c206a7ec06
martin patel <martinpatel610@gmail.com>
2018-05-02 19:46:28 -0400

    first git commit

---
+++ src/main/java/qa/com/Login.java
@@ -0,0 +1,5 @@
+package qa.com;
+
+public class Login {
+
+}
---
+++ src/test/java/qa/tests/Testlogin.java
@@ -0,0 +1,13 @@
+package qa.tests;
+
+import org.testng.annotations.Test;
+
+public class Testlogin {
+	
+	@Test
+	public void testlogin()
+	{
+		System.out.println("test login");
+	}
+
+}
