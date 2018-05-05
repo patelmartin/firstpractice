b011e7cb6c78ebd30304d8d9d984fc667d866700
martin <martinpatel610@gmail.com>
2018-05-05 16:52:21 -0400

    first change today

--- src/main/java/qa/com/Login.java
+++ src/main/java/qa/com/Login.java
@@ -10,5 +10,12 @@ public class Login {
 	{
 		System.out.println("login set");
 	}
-
+	public void martin()
+	{
+		System.out.println("martin is ready");
+	}
+	public void first()
+	{
+		System.out.println("first break up");
+	}
 }
